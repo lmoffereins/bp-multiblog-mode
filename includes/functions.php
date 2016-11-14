23eb55db71ed8792cac5132b5169edfebf7e900b
Laurens Offereins <lmoffereins@gmail.com>
2016-11-14 16:41:22 +0100

    Allow additional query arguments when querying the network's (enabled) sites

--- includes/functions.php
+++ includes/functions.php
@@ -81,21 +81,51 @@ function bp_multiblog_mode_is_enabled( $site_id = 0 ) {
  * @param bool $enabled Optional. Whether to return only enabled sites. Defaults to false.
  * @return array Sites, maybe only enabled
  */
-function bp_multiblog_mode_get_sites( $enabled = false ) {
+function bp_multiblog_mode_get_sites( $args = array() ) {
+
+	// Provide a fallback for non-array parameter
+	if ( ! is_array( $args ) ) {
+		$args = array( 'multiblog' => (bool) $args );
+	}
+
+	// Define query args
+	$args = wp_parse_args( $args, array(
+		'network_id' => get_network()->id,
+		'multiblog'  => false
+	) );
+
+	// Query Multiblog-only?
+	$multiblog = (bool) $args['multiblog'];
+	unset( $args['multiblog'] );
 
 	// Get the Network's sites
-	$sites = get_sites( array( 'network_id' => get_network()->id ) );
+	$sites = get_sites( $args );
 
-	// Filter for enabled sites
-	if ( $enabled ) {
+	// Filter for enabled Multiblog sites
+	if ( $multiblog ) {
 		foreach ( $sites as $k => $site ) {
 			
 			// Remove the unenabled site
-			if ( ! bp_multiblog_mode_is_enabled( $site->id ) ) {
+			if ( ! bp_multiblog_mode_is_enabled( is_a( $site, 'WP_Site' ) ? $site->id : (int) $site ) ) {
 				unset( $sites[ $k ] );
 			}
 		}
 	}
 
 	return $sites;
+}
+
+/**
+ * Return the Multiblog enabled sites of the current network
+ *
+ * @see bp_multiblog_mode_get_sites()
+ *
+ * @since 1.0.0
+ *
+ * @param array Optional. Query arguments for WP_Site_Query.
+ * @return array Multiblog enabled sites
+ */
+function bp_multiblog_mode_get_enabled_sites( $args = array() ) {
+	$args = wp_parse_args( $args, array( 'multiblog' => true ) );
+	return bp_multiblog_mode_get_sites( $args );
 }
